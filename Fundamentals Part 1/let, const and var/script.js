e8b5df016a116ca25b7cfa3230352389e5f5e28b
Jonibek Toirov <60321513+Jonibek01@users.noreply.github.com>
2021-06-24 22:47:24 +0500

    Create script.js

---
+++ Fundamentals Part 1/let, const and var/script.js
@@ -0,0 +1,21 @@
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+// we gotta use the const always and let when we need to change the variable later, otherwise use const
+
+// never use var!
+
+// *let is block scoped element
+
+// var is function scoped element
