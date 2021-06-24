0ad7352ddd2ad1a58aad356a9c2a72a11e237062
Jonibek Toirov <60321513+Jonibek01@users.noreply.github.com>
2021-06-24 22:46:47 +0500

    Update script.js

--- Fundamentals Part 1/Data types/script.js
+++ Fundamentals Part 1/Data types/script.js
@@ -1,1 +1,27 @@
 
+
+////////////////////////////////////
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// declaring second time which no need to store in variable one more time
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+// undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+// number
+year = 1991;
+console.log(typeof year);
+
+// object
+console.log(typeof null);
